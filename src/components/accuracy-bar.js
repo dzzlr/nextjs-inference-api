9728d22e6182fd6ba4750ef6b7bfb4d6ae62fac6
dzzlr <inigithudazzler@gmail.com>
2023-03-25 14:55:02 +0700

    edit some post text and response ui

--- src/components/accuracy-bar.js
+++ src/components/accuracy-bar.js
@@ -1,12 +1,13 @@
-export default function AccuracyBar({ label, value, }) {
+export default function AccuracyBar({ label, value }) {
   return (
-    <div className="">
-      <div className="flex justify-between">
+    <div>
+      <div className="mt-1 flex justify-between">
         <h5 className="text-slate-500 font-medium font-mono text-xs">{label}</h5>
         <h5 className="text-slate-400 font-light font-mono text-xs">{value}%</h5>
       </div>
-      <div className="mt-1 w-full h-2 rounded-md bg-slate-100 transition-all"></div>
-      <div className={`w-[${value}%] h-2 rounded-md bg-indigo-400 -translate-y-2 transition-all`}></div>
+      <div className="mt-1 w-full h-2 rounded-md bg-slate-100">
+        <div className={`w-[${value}%] h-2 rounded-md bg-indigo-400`}></div>
+      </div>
     </div>
   )
 }
--- src/pages/predict-news-title.js
+++ src/pages/predict-news-title.js
@@ -2,91 +2,32 @@ import TextArea from "@/components/textarea";
 import Button from "@/components/button";
 import AccuracyBar from "@/components/accuracy-bar";
 import { useState } from "react";
-import { axios } from "axios";
+import axios from "axios";
 
 export default function PredictNewsTitle() {
   const [showJSON,setShowJSON] = useState(false);
   const [text,setText] = useState('');
-
-  // const addTask = () => {
-  //   // 👇 Send a fetch request to Backend API.
-  //   fetch("http://127.0.0.1:8000/predict-news", {
-  //     method: "POST",
-  //     mode: 'no-cors',
-  //     body: JSON.stringify({
-  //       text: "Kylian Mbappe akan segera bergabung dengan Real Madrid pada bursa transfer musim dingin mendatang",
-  //     }),
-  //     headers: {
-  //       // 'Access-Control-Allow-Origin': "*",
-  //       // 'Access-Control-Allow-Methods': "POST",
-  //       'Content-type': 'application/json; charset=UTF-8',
-  //     },
-  //   })
-  //   .then(function(response){ 
-  //     return response.json()})
-  //   .then(function(data) {
-  //     console.log(data)})
-  //   .catch(error => console.error('Error:', error)); 
-  // };
+  const [results, setResults] = useState([]);
 
   const submitTextTitle = async () => {
-    console.log(text)
-    // axios
-    //   .post('http://127.0.0.1:8000/predict-news', {text: "Charles Leclerc masih kesulitan dalam mengangkat performa Ferrari pada musim ini"})
-    //   .then((resp) => {
-    //     console.log(resp)
-    //   })
-    //   .catch((err) => {
-    //     console.log(err);
-    //   });
-
     try {
-      const response = await fetch('http://127.0.0.1:8000/predict-news', {
-        method: 'POST',
-        mode: 'no-cors',
+      const res = await axios.post('http://127.0.0.1:8000/predict-news', {
+        text: text
+      }, {
+        timeout: 8000,
         headers: {
-          'Origin': 'http://localhost:3000/',
-          'Access-Control-Allow-Methods': 'POST',
-          'Access-Control-Allow-Credentials': 'true',
-          'Access-Control-Allow-Headers': 'Content-Type',
-          'Content-Type': 'application/json',
-          'Accept': 'application/json'
+          'Content-Type': 'application/json'
         },
-        body: JSON.stringify({text: "Charles Leclerc masih kesulitan dalam mengangkat performa Ferrari pada musim ini"})
-      });
-      const data = await response.json();
-
-      if (!response.ok) {
-        // throw new Error(`Error! status: ${response.status}`);
-        console.log(data);
-        return;
-      }
-      console.log(data);
-    } catch (error) {
-      console.log(error)
+      },
+      )
+      const data_response = await res;
+      const {result} = data_response.data.data;
+      // console.log(result);
+      setResults(result);
+    } catch (e) {
+      console.log(e);
     }
-
-    // try {
-    //   const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
-    //     method: 'GET',
-    //     // mode: 'no-cors',
-    //     headers: {
-    //       // 'Access-Control-Allow-Origin': 'POST',
-    //       'Content-type': 'application/json'
-    //     }
-    //     // body: JSON.stringify({text: "Charles Leclerc masih kesulitan dalam mengangkat performa Ferrari pada musim ini"})
-    //   });
-    //   const data = await response.json();
-
-    //   if (!response.ok) {
-    //     // throw new Error(`Error! status: ${response.status}`);
-    //     console.log(data);
-    //     return;
-    //   }
-    //   console.log(data);
-    // } catch (error) {
-    //   console.log(error)
-    // }
+    // console.log(results);
   }
 
 
@@ -113,7 +54,6 @@ export default function PredictNewsTitle() {
               <div className="w-full p-5 rounded-lg border border-slate-200 shadow-md">
                 <h3 className="text-base text-black font-bold mb-1">model/svm-tfidf</h3>
                 <h4 className="text-sm font-normal text-slate-400 mb-1">Text Classification</h4>
-                {/* <div className="mt-3 flex flex-col items-start" onSubmit={(e) => {e.preventDefault(); addTask();}}> */}
                 <div className="mt-3 flex flex-col items-start">
 
                   <TextArea 
@@ -132,18 +72,17 @@ export default function PredictNewsTitle() {
                 </div>
                 <p className="mb-4 text-slate-400 text-xs">Computation time on Intel Core i3 7th Gen Scalable cpu: cached and Nvidia GTX 1050Ti</p>
 
-                <div className="">
-                  <AccuracyBar label="finance" value="0.7"/>
-                  <AccuracyBar label="food" value="0.3"/>
-                  <AccuracyBar label="health" value="0.2"/>
-                  <AccuracyBar label="inet" value="0.4"/>
-                  <AccuracyBar label="oto" value="1"/>
-                  <AccuracyBar label="sport" value="95.9"/>
-                  <AccuracyBar label="travel" value="1.4"/>
-                </div>
+                { results.length > 0 ? results.map((result, i) => {
+                  return (
+                    <div key={i}>
+                      <AccuracyBar label={result.label} value={(result.score * 100).toFixed(2)}/>
+                    </div>
+                  )
+                }) : null }
+
 
                 <button 
-                  onClick={()=>{
+                  onClick={() => {
                     setShowJSON(!showJSON);
                   }}
                   className="mt-10 text-slate-400 text-xs font-mono transition duration-150 ease-out"
@@ -154,10 +93,10 @@ export default function PredictNewsTitle() {
                 { 
                   showJSON ? 
                   <div className="mt-2 w-full p-3 bg-slate-100 text-slate-500 text-xs font-mono rounded-lg transition duration-150 ease-out">
-                    Computation time on Intel Core i3 7th Gen Scalable cpu: cached and Nvidia GTX 1050Ti 
+                    {/* { results.length > 0 ? results : null } */}
+                    {JSON.stringify(results)}
                   </div>
-                  : null
-                }
+                  : null }
               </div>
             </div>
           </div>
