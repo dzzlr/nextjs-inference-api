27526321769e60d24f5f3c9d8014b967bf1bce37
dzzlr <inigithudazzler@gmail.com>
2023-03-27 08:33:06 +0700

    changing arbitrary value with style attribute of width

--- src/components/accuracy-bar.js
+++ src/components/accuracy-bar.js
@@ -1,13 +1,13 @@
 export default function AccuracyBar({ label, value }) {
   return (
-    <div>
-      <div className="mt-1 flex justify-between">
+    <>
+      <div className="mt-1 flex justify-between group transition-all duration-500 ease-out">
         <h5 className="text-slate-500 font-medium font-mono text-xs">{label}</h5>
         <h5 className="text-slate-400 font-light font-mono text-xs">{value}%</h5>
       </div>
-      <div className="mt-1 w-full h-2 rounded-md bg-slate-100">
-        <div className={`w-[${value}%] h-2 rounded-md bg-indigo-400`}></div>
+      <div className="mt-1 w-full h-2 rounded-md bg-slate-100 group">
+        <div className="h-2 rounded-md bg-indigo-400" style={{width: `${value}%`}}></div>
       </div>
-    </div>
+    </>
   )
 }
