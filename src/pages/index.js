1e5c0e0b5b3672f59b34d8dfa9be66a543ef3a2c
dzzlr <inigithudazzler@gmail.com>
2023-03-28 17:28:45 +0700

    Refactoring fetching data with getStaticProps

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,9 @@
 import Layout from "@/components/Layout"
 import CardTask from "@/components/card-task"
 import Badge from "@/components/badge"
-import { useState, useEffect } from 'react'
 
-export default function Home() {
-  const [data, setData] = useState(null)
-  const [isLoading, setLoading] = useState(false)
-  
-  useEffect(() => {
-    setLoading(true)
-    fetch('/api/tasks')
-      .then((res) => res.json())
-      .then((data) => {
-        setData(data)
-        setLoading(false)
-      })
-  }, [])
-
-  if (isLoading) return <p>Loading...</p>
-  if (!data) return <p>No tasks data</p>
+export default function Home(props) {
+  const {tasks} = props;
 
   return (
     <>
@@ -28,25 +13,36 @@ export default function Home() {
             <h1 className="mb-5 lg:mb-10 text-lg md:text-2xl text-black font-extrabold">Welcome to ML Playground (API Inference)</h1>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 lg:gap-6">
               
-              { data.length > 0 ? data.map((result, i) => {
+              { tasks.map(task => {
                 return (
-                  <CardTask href={result.url} key={i}>
+                  <CardTask href={task.url} key={task._id}>
                     <div className="flex flex-row">
-                      { result.category == 'NLP' ? <Badge className={'bg-yellow-100 text-yellow-600'}>{result.category}</Badge> : null }
-                      { result.category == 'OCR' ? <Badge className={'bg-blue-100 text-blue-800'}>{result.category}</Badge> : null }
-                      { result.status == 'Ready' ? <Badge className={'bg-green-100 text-green-800'}>{result.status}</Badge> : null }
-                      { result.status == 'On Develop' ? <Badge className={'bg-red-100 text-red-800'}>{result.status}</Badge> : null }
+                      { task.category == 'NLP' ? <Badge className={'bg-yellow-100 text-yellow-600'}>{task.category}</Badge> : null }
+                      { task.category == 'OCR' ? <Badge className={'bg-blue-100 text-blue-800'}>{task.category}</Badge> : null }
+                      { task.status == 'Ready' ? <Badge className={'bg-green-100 text-green-800'}>{task.status}</Badge> : null }
+                      { task.status == 'On Develop' ? <Badge className={'bg-red-100 text-red-800'}>{task.status}</Badge> : null }
                     </div>
                     <div className="w-full h-20"></div>
-                    <h3 className="text-base text-black font-bold">{result.name}</h3>
-                    <h4 className="text-sm font-normal text-slate-400">{result.task}</h4>
+                    <h3 className="text-base text-black font-bold">{task.name}</h3>
+                    <h4 className="text-sm font-normal text-slate-400">{task.task}</h4>
                   </CardTask>
                 )
-              }) : null }
+              })}
+              
             </div>
           </div>
         </div>
       </Layout>
     </>
   )
+}
+
+export async function getStaticProps(context) {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}api/tasks`);
+  const tasks = await res.json();
+  return {
+    props: {
+      tasks,
+    },
+  }
 }
